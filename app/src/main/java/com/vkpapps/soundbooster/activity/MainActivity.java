2f7806f1a180951531bc4e90f4d1a576194d14a0
Vijay Patidar <41512842+vramotiya@users.noreply.github.com>
2020-03-13 09:33:58 +0530

    screen orientation fix direction

--- app/src/main/java/com/vkpapps/soundbooster/activity/MainActivity.java
+++ app/src/main/java/com/vkpapps/soundbooster/activity/MainActivity.java
@@ -1,19 +1,16 @@
 package com.vkpapps.soundbooster.activity;
 
 import android.content.Intent;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.vkpapps.soundbooster.R;
 import com.vkpapps.soundbooster.handler.SignalHandler;
 import com.vkpapps.soundbooster.utils.PermissionUtils;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,28 +28,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         intent = new Intent(this, PartyActivity.class);
-        createHost();
-    }
 
-
-    private void setup() {
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-
-        Method[] wmMethods = wifiManager.getClass().getDeclaredMethods();
-        for (Method method : wmMethods) {
-            if (method.getName().equals("isWifiApEnabled")) {
-                try {
-                    boolean isWifiAPEnabled = (Boolean) method.invoke(wifiManager);
-                    if (isWifiAPEnabled) {
-                        createHost();
-                    } else {
-                        connectToHost();
-                    }
-                } catch (IllegalArgumentException | InvocationTargetException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        AlertDialog.Builder ab = new AlertDialog.Builder(this);
+        ab.setTitle("what you want to be...");
+        ab.setPositiveButton("host", (dialog, which) -> {
+            createHost();
+        });
+        ab.setNegativeButton("member", (dialog, which) -> {
+            connectToHost();
+        });
+        ab.create().show();
     }
 
     private void createHost() {
@@ -63,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void connectToHost() {
-        String host = "192.168.43.1";
             intent.putExtra("isHost", false);
-            intent.putExtra("host", host);
+        startActivity(intent);
+        finish();
     }
 }
