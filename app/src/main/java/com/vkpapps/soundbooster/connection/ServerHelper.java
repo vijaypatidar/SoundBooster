59914d69d7b0a0169f97fece468653e553c86405
Vijay Patidar <41512842+vramotiya@users.noreply.github.com>
2020-03-13 09:18:29 +0530

    command server helper class created

---
+++ app/src/main/java/com/vkpapps/soundbooster/connection/ServerHelper.java
@@ -0,0 +1,41 @@
+package com.vkpapps.soundbooster.connection;
+
+import com.vkpapps.soundbooster.handler.SignalHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerHelper implements Runnable {
+    private SignalHandler signalHandler;
+    private ArrayList<CommandHelperRunnable> commandHelperRunnables;
+
+    public ServerHelper(SignalHandler signalHandler) {
+        this.signalHandler = signalHandler;
+        commandHelperRunnables = new ArrayList<>();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                ServerSocket serverSocket = new ServerSocket(1203);
+                Socket socket = serverSocket.accept();
+                CommandHelperRunnable commandHelper = new CommandHelperRunnable(socket, signalHandler);
+                commandHelperRunnables.add(commandHelper);
+                new Thread(commandHelper).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendCommand(String command) {
+        new Thread(() -> {
+            for (CommandHelperRunnable c : commandHelperRunnables) {
+                c.write(command);
+            }
+        }).start();
+    }
+}
