5ce04a42d332d778be3df0ac98ed6307b4242e99
Vijay Patidar <41512842+vijaypatidar31@users.noreply.github.com>
2020-04-21 20:10:33 +0530

    auto detect gateway address

--- app/src/main/java/com/vkpapps/soundbooster/utils/Utils.java
+++ app/src/main/java/com/vkpapps/soundbooster/utils/Utils.java
@@ -142,4 +142,12 @@ public class Utils {
             e.printStackTrace();
         }
     }
+
+    public static String intToIp(int i) {
+
+        return (i & 0xFF) + "." +
+                ((i >> 8) & 0xFF) + "." +
+                ((i >> 16) & 0xFF) + "." +
+                ((i >> 24) & 0xFF);
+    }
 }
